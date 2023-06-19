07853e646a12c79420fe7b775eafaf5a40d2682a
FaresX <fyzxst@sina.com>
2023-06-19 17:16:57 +0800

    fix bugs

--- precompile.jl
+++ precompile.jl
@@ -1,6 +1,7 @@
 # using Distributed
 # nprocs() == 1 && addprocs(1)
 # @everywhere using Pkg
+ENV["QInsControlAssets"] = joinpath(Base.@__DIR__, "Assets")
 using Pkg
 Pkg.activate(Base.@__DIR__)
 using QInsControl
