822380ba5f6dbf2964d244a41ee834cb57f53d80
FaresX <fyzxst@sina.com>
2023-06-27 09:53:40 +0800

    modified:   deps/build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,4 +1,4 @@
-conf_path = joinpath(dirname(pathof(QInsControl)), "../Assets/Necessity/conf.toml")
-insconf_paths = readdir(joinpath(dirname(pathof(QInsControl)), "../Assets/Confs"))
+conf_path = joinpath(Base.@__DIR__, "../Assets/Necessity/conf.toml")
+insconf_paths = readdir(joinpath(Base.@__DIR__, "../Assets/Confs"))
 run(`attrib -R $conf_path`)
 for insconf_path in insconf_paths run(`attrib -R $insconf_path`) end
