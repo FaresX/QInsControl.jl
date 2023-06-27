0d2afa3559931e26c394980d02593641fdddd80a
FaresX <fyzxst@sina.com>
2023-06-27 11:58:32 +0800

    modified:   deps/build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,8 @@
 conf_path = joinpath(Base.@__DIR__, "../Assets/Necessity/conf.toml")
 insconf_paths = readdir(joinpath(Base.@__DIR__, "../Assets/Confs"))
 run(`attrib -R $conf_path`)
-run(`CACLS $conf_path /p Everyone:W`)
+run(`Icacls $conf_path /grant Everyone:F`)
 for insconf_path in insconf_paths
     run(`attrib -R $insconf_path`)
-    run(`CACLS $insconf_path /p Everyone:W`)
+    run(`Icacls $insconf_path /grant Everyone:F`)
 end
