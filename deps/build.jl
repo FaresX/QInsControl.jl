f2f70ade27a318a5f85b4e9fc2c160f6a372fc53
FaresX <fyzxst@sina.com>
2023-06-27 16:16:07 +0800

    modified:   deps/build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,8 @@
-conf_path = joinpath(Base.@__DIR__, "../Assets/Necessity/conf.toml")
+# conf_path = joinpath(Base.@__DIR__, "../Assets/Necessity/conf.toml")
 insconf_paths = readdir(joinpath(Base.@__DIR__, "../Assets/Confs"); join=true)
 if Sys.iswindows()
-    run(`attrib -R $conf_path`)
-    run(`Icacls $conf_path /grant Everyone:F`)
+    # run(`attrib -R $conf_path`)
+    # run(`Icacls $conf_path /grant Everyone:F`)
     for insconf_path in insconf_paths
         run(`attrib -R $insconf_path`)
         run(`Icacls $insconf_path /grant Everyone:F`)
