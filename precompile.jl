d7858db79d0b79fa052513c432a29daff0fcb969
FaresX <fyzxst@sina.com>
2023-08-04 11:13:31 +0800

    remove something unrelevant

--- precompile.jl
+++
@@ -1,9 +0,0 @@
-# using Distributed
-# nprocs() == 1 && addprocs(1)
-# @everywhere using Pkg
-ENV["QInsControlAssets"] = joinpath(Base.@__DIR__, "Assets")
-using Pkg
-Pkg.activate(Base.@__DIR__)
-using QInsControl
-# julia_main()
-QInsControl.start()
