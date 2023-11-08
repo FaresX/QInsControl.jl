97c0f90faba3804a47b2f0ca9a828bfd409943fd
FaresX <fyzxst@sina.com>
2023-11-08 16:33:20 +0800

    widget for friendly user interface

--- src/UI/InstrWidget.jl
+++ src/UI/InstrWidget.jl
@@ -1,36 +1,42 @@
-@option mutable struct QuantityControlOption
+@option mutable struct QuantityWidgetOption
     uitype::String
     textcolor::Vector{Cfloat} = [1.000, 1.000, 1.000, 1.000]
 end
 
-abstract type AbstractQuantityControl end
+abstract type AbstractQuantityWidget end
 
-@option struct NullQuantityControl <: AbstractQuantityControl end
+@option struct NullQuantityWidget <: AbstractQuantityWidget end
 
-@option mutable struct SweepQuantityControl <: AbstractQuantityControl
+@option mutable struct SweepQuantityWidget <: AbstractQuantityWidget
     name::String = ""
-    options::QuantityControlOption = QuantityControlOption()
+    alias::String = ""
+    options::QuantityWidgetOption = QuantityWidgetOption("button")
 end
 
-@option mutable struct SetQuantityControl <: AbstractQuantityControl
+@option mutable struct SetQuantityWidget <: AbstractQuantityWidget
     name::String = ""
-    options::QuantityControlOption = QuantityControlOption()
+    alias::String = ""
+    options::QuantityWidgetOption = QuantityWidgetOption("combo")
 end
 
-@option mutable struct ReadQuantityControl <: AbstractQuantityControl
+@option mutable struct ReadQuantityWidget <: AbstractQuantityWidget
     name::String = ""
-    options::QuantityControlOption = QuantityControlOption()
+    alias::String = ""
+    options::QuantityWidgetOption = QuantityWidgetOption("text")
 end
 
-@option mutable struct InstrControl
-    instrnm::String
+@option mutable struct InstrWidget
+    instrnm::String = "VirtualInstr"
     cols::Cint = 1
-    qtctls::Vector{AbstractQuantityControl} = []
+    qtws::Vector{AbstractQuantityWidget} = []
 end
 
-edit(::NullQuantityControl, _, _) = CImGui.Text("")
+qtwtoqt(qtw::AbstractQuantityWidget, instrnm, addr) = INSTRBUFFERVIEWERS[instrnm][addr].insbuf.quantities[qtw.name]
 
-function edit(qt::SweepQuantityControl, instrnm, addr)
+edit(::NullQuantityWidget, _, _) = CImGui.Text("")
+
+function edit(qtw::SweepQuantityWidget, instrnm, addr)
+    qt = qtwtoqt(qtw, instrnm, addr)
     CImGui.TextColored(options["textcolor"], qt.read)
     @c InputTextWithHintRSZ("##step", mlstr("step"), &qt.step)
     @c InputTextWithHintRSZ("##stop", mlstr("stop"), &qt.stop)
@@ -42,7 +48,8 @@ function edit(qt::SweepQuantityControl, instrnm, addr)
     end
 end
 
-function edit(qt::SetQuantityControl, instrnm, addr)
+function edit(qtw::SetQuantityWidget, instrnm, addr)
+    qt = qtwtoqt(qtw, instrnm, addr)
     if qt.options.uitype == "combo"
         presentv = qt.optkeys[qt.optedidx]
         if @c ComBoS(qt.alias, &presentv, qt.optkeys)
@@ -60,37 +67,83 @@ function edit(qt::SetQuantityControl, instrnm, addr)
     end
 end
 
-function edit(qt::ReadQuantityControl, _, _)
+function edit(qtw::ReadQuantityWidget, _, _)
+    qt = qtwtoqt(qtw, instrnm, addr)
     CImGui.TextColored(qt.options.textcolor, qt.read)
 end
 
-function edit(insctl::InstrControl, addr)
-    CImGui.Columns(insctl.cols)
-    for qt in insctl.qtctls
-        edit(qt, insctl.instrnm, addr)
+function edit(insw::InstrWidget, addr)
+    CImGui.Columns(insw.cols)
+    for qtw in insw.qtws
+        edit(qtw, insw.instrnm, addr)
         CImGui.NextColumn()
     end
 end
 
-function modify(qt::AbstractQuantityControl)
-    CImGui.Button(qt.name)
+function modify(qtw::SweepQuantityWidget)
+    CImGui.Button(qtw.alias, (-1, 0))
     if CImGui.BeginPopupContextItem()
         CImGui.EndPopup()
     end
 end
 
-mutable struct InstrControlViewer
-    instrnm::String
-    addr::String
-    p_open::Bool
-    insctl::InstrControl
+function modify(qtw::SetQuantityWidget)
+    CImGui.Button(qtw.alias, (-1, 0))
+    if CImGui.BeginPopupContextItem()
+        CImGui.EndPopup()
+    end
 end
 
-function ShowInstrWidget(icv::InstrControlViewer)
+function modify(qtw::ReadQuantityWidget)
+    CImGui.Button(qtw.alias, (-1, 0))
+    if CImGui.BeginPopupContextItem()
+        CImGui.EndPopup()
+    end
+end
+
+function modify(insw::InstrWidget)
+    CImGui.Columns(insw.cols)
+    for (i, qtw) in enumerate(insw.qtws)
+        modify(qtw)
+        CImGui.NextColumn()
+        CImGui.Indent()
+        if CImGui.BeginDragDropSource(0)
+            @c CImGui.SetDragDropPayload("Swap Widgets", &i, sizeof(Cint))
+            CImGui.Text(qtw.alias)
+            CImGui.EndDragDropSource()
+        end
+        if CImGui.BeginDragDropTarget()
+            payload = CImGui.AcceptDragDropPayload("Swap DAQTask")
+            if payload != C_NULL && unsafe_load(payload).DataSize == sizeof(Cint)
+                payload_i = unsafe_load(Ptr{Cint}(unsafe_load(payload).Data))
+                if i != payload_i
+                    insw.qtws[i] = insw.qtws[payload_i]
+                    insw.qtws[payload_i] = qtw
+                end
+            end
+            CImGui.EndDragDropTarget()
+        end
+        CImGui.Unindent()
+    end
+end
+
+@kwdef mutable struct InstrWidgetViewer
+    instrnm::String = "VirtualInstr"
+    addr::String = "VirtualAddress"
+    p_open::Bool = false
+    ismodify::Bool = false
+    insw::InstrWidget = InstrWidget()
+end
+
+function edit(iwv::InstrWidgetViewer)
     CImGui.SetNextWindowSize((800, 600), CImGui.ImGuiCond_Once)
-    ins, addr = icv.instrnm, icv.addr
-    if @c CImGui.Begin(stcstr(INSCONF[ins].conf.icon, "  ", ins, " --- ", addr), &icv.p_open)
-        edit(icv.insctl, addr)
+    ins, addr = iwv.instrnm, iwv.addr
+    if @c CImGui.Begin(stcstr(INSCONF[ins].conf.icon, "  ", ins, " --- ", addr), &iwv.p_open)
+        iwv.ismodify ? modify(iwv.insw) : edit(iwv.insw, addr)
     end
     CImGui.End()
+    if CImGui.BeginPopupContextWindow()
+        @c CImGui.Checkbox(mlstr(iwv.ismodify ? "Edit" : "View"), &iwv.ismodify)
+        CImGui.EndPopup()
+    end
 end
