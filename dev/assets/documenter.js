f655c18f8b3c846bdfdc0113a694bb3bd1dae512
Documenter.jl <documenter@juliadocs.github.io>
2024-01-10 06:20:56 +0000

    build based on d19f7f0

--- dev/assets/documenter.js
+++ dev/assets/documenter.js
@@ -75,75 +75,60 @@ $(document).ready(function() {
 ////////////////////////////////////////////////////////////////////////////////
 require(['jquery'], function($) {
 
-let timer = 0;
 var isExpanded = true;
 
 $(document).on("click", ".docstring header", function () {
   let articleToggleTitle = "Expand docstring";
 
-  debounce(() => {
-    if ($(this).siblings("section").is(":visible")) {
-      $(this)
-        .find(".docstring-article-toggle-button")
-        .removeClass("fa-chevron-down")
-        .addClass("fa-chevron-right");
-    } else {
-      $(this)
-        .find(".docstring-article-toggle-button")
-        .removeClass("fa-chevron-right")
-        .addClass("fa-chevron-down");
-
-      articleToggleTitle = "Collapse docstring";
-    }
-
+  if ($(this).siblings("section").is(":visible")) {
     $(this)
       .find(".docstring-article-toggle-button")
-      .prop("title", articleToggleTitle);
-    $(this).siblings("section").slideToggle();
-  });
+      .removeClass("fa-chevron-down")
+      .addClass("fa-chevron-right");
+  } else {
+    $(this)
+      .find(".docstring-article-toggle-button")
+      .removeClass("fa-chevron-right")
+      .addClass("fa-chevron-down");
+
+    articleToggleTitle = "Collapse docstring";
+  }
+
+  $(this)
+    .find(".docstring-article-toggle-button")
+    .prop("title", articleToggleTitle);
+  $(this).siblings("section").slideToggle();
 });
 
 $(document).on("click", ".docs-article-toggle-button", function () {
   let articleToggleTitle = "Expand docstring";
   let navArticleToggleTitle = "Expand all docstrings";
 
-  debounce(() => {
-    if (isExpanded) {
-      $(this).removeClass("fa-chevron-up").addClass("fa-chevron-down");
-      $(".docstring-article-toggle-button")
-        .removeClass("fa-chevron-down")
-        .addClass("fa-chevron-right");
+  if (isExpanded) {
+    $(this).removeClass("fa-chevron-up").addClass("fa-chevron-down");
+    $(".docstring-article-toggle-button")
+      .removeClass("fa-chevron-down")
+      .addClass("fa-chevron-right");
 
-      isExpanded = false;
+    isExpanded = false;
 
-      $(".docstring section").slideUp();
-    } else {
-      $(this).removeClass("fa-chevron-down").addClass("fa-chevron-up");
-      $(".docstring-article-toggle-button")
-        .removeClass("fa-chevron-right")
-        .addClass("fa-chevron-down");
+    $(".docstring section").slideUp();
+  } else {
+    $(this).removeClass("fa-chevron-down").addClass("fa-chevron-up");
+    $(".docstring-article-toggle-button")
+      .removeClass("fa-chevron-right")
+      .addClass("fa-chevron-down");
 
-      isExpanded = true;
-      articleToggleTitle = "Collapse docstring";
-      navArticleToggleTitle = "Collapse all docstrings";
+    isExpanded = true;
+    articleToggleTitle = "Collapse docstring";
+    navArticleToggleTitle = "Collapse all docstrings";
 
-      $(".docstring section").slideDown();
-    }
-
-    $(this).prop("title", navArticleToggleTitle);
-    $(".docstring-article-toggle-button").prop("title", articleToggleTitle);
-  });
-});
-
-function debounce(callback, timeout = 300) {
-  if (Date.now() - timer > timeout) {
-    callback();
+    $(".docstring section").slideDown();
   }
 
-  clearTimeout(timer);
-
-  timer = Date.now();
-}
+  $(this).prop("title", navArticleToggleTitle);
+  $(".docstring-article-toggle-button").prop("title", articleToggleTitle);
+});
 
 })
 ////////////////////////////////////////////////////////////////////////////////
