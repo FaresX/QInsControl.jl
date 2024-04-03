8803f6b8de90ee5596b8cdc96c9fad92a2006822
FaresX <fyzxst@sina.com>
2024-04-03 13:38:50 +0800

    rewrite CPUMonitor

--- src/UI/CPUMonitor.jl
+++ src/UI/CPUMonitor.jl
@@ -1,104 +1,100 @@
 function CPUMonitor()
-    # CImGui.SetNextWindowSize((400, 200), CImGui.ImGuiCond_Once)
-    # if CImGui.Begin(stcstr(MORESTYLE.Icons.CPUMonitor, " ", mlstr("Instrument CPU Monitor"), "###CPU"), p_open)
-    #     SetWindowBgImage()
-    CImGui.TextColored(MORESTYLE.Colors.HighlightText, "ID: ")
-    CImGui.SameLine()
-    CImGui.Text(stcstr(remotecall_fetch(() -> CPU.id, workers()[1])))
-    CImGui.Spacing()
-    SeparatorTextColored(MORESTYLE.Colors.HighlightText, mlstr("Status"))
+    cpuinfo = remotecall_fetch(workers()[1]) do
+        Dict(
+            :running => CPU.running[],
+            :taskfailed => istaskfailed(CPU.processtask[]),
+            :fast => CPU.fast[],
+            :resourcemanager => CPU.resourcemanager[],
+            :isconnected => Dict(addr => QInsControlCore.isconnected(instr) for (addr, instr) in CPU.instrs),
+            :controllers => CPU.controllers,
+            :taskhandlers => CPU.taskhandlers,
+            :tasksfailed => Dict(addr => istaskfailed(task) for (addr, task) in CPU.tasks)
+        )
+    end
+    SeparatorTextColored(MORESTYLE.Colors.HighlightText, mlstr("Processor"))
     CImGui.Indent()
-    if remotecall_fetch(() -> CPU.running, workers()[1])[]
-        # CImGui.TextColored(MORESTYLE.Colors.LogInfo, mlstr("running"))
-        # CImGui.SameLine()
-        if SYNCSTATES[Int(IsDAQTaskRunning)]
-            CImGui.PushStyleColor(
-                CImGui.ImGuiCol_Text,
-                CImGui.c_get(IMGUISTYLE.Colors, CImGui.ImGuiCol_TextDisabled)
-            )
-        end
-        if ToggleButton(mlstr("Running"), Ref(true))
-            SYNCSTATES[Int(IsDAQTaskRunning)] || remotecall_wait(() -> stop!(CPU), workers()[1])
-        end
-        SYNCSTATES[Int(IsDAQTaskRunning)] && CImGui.PopStyleColor()
+    if cpuinfo[:running]
+        igBeginDisabled(SYNCSTATES[Int(IsDAQTaskRunning)])
+        ToggleButton(mlstr("Running"), Ref(true)) && remotecall_wait(() -> stop!(CPU), workers()[1])
+        igEndDisabled()
         CImGui.SameLine()
-        if remotecall_fetch(() -> CPU.fast, workers()[1])[]
-            # CImGui.Text(mlstr("Fast Mode"))
-            CImGui.Checkbox(mlstr("Fast Mode"), Ref(true)) && remotecall_wait(() -> CPU.fast[] = false, workers()[1])
-        else
-            # CImGui.Text(mlstr("Slow Mode"))
-            CImGui.Checkbox(mlstr("Slow Mode"), Ref(false)) && remotecall_wait(() -> CPU.fast[] = true, workers()[1])
+        ColoredButton(
+            mlstr(cpuinfo[:taskfailed] ? "Failed" : "Well");
+            colbt=cpuinfo[:taskfailed] ? MORESTYLE.Colors.LogError : MORESTYLE.Colors.LogInfo,
+            colbth=cpuinfo[:taskfailed] ? MORESTYLE.Colors.LogError : MORESTYLE.Colors.LogInfo,
+            colbta=cpuinfo[:taskfailed] ? MORESTYLE.Colors.LogError : MORESTYLE.Colors.LogInfo
+        )
+        CImGui.SameLine()
+        if CImGui.Checkbox(mlstr(cpuinfo[:fast] ? "Fast Mode" : "Slow Mode"), Ref(cpuinfo[:fast]))
+            remotecall_wait((isfast) -> CPU.fast[] = !isfast, workers()[1], cpuinfo[:fast])
         end
-        # CImGui.SameLine()
-        # if CImGui.Button(stcstr(MORESTYLE.Icons.Convert, " ", mlstr("Switch")))
-        #     remotecall_wait(workers()[1]) do
-        #         CPU.fast[] ⊻= true
-        #     end
-        # end
     else
-        # CImGui.TextColored(MORESTYLE.Colors.LogError, mlstr("stopped"))
-        # CImGui.SameLine()
-        if ToggleButton(mlstr("Stopped"), Ref(false))
-            remotecall_wait(() -> start!(CPU), workers()[1])
-        end
+        ToggleButton(mlstr("Stopped"), Ref(false)) && remotecall_wait(() -> start!(CPU), workers()[1])
     end
     CImGui.Unindent()
     CImGui.Spacing()
-    SeparatorTextColored(MORESTYLE.Colors.HighlightText, mlstr("controllers"))
+    SeparatorTextColored(MORESTYLE.Colors.HighlightText, mlstr("Controllers"))
     CImGui.Indent()
     CImGui.Button(
         stcstr(MORESTYLE.Icons.InstrumentsManualRef, " ", mlstr("Reconnect"))
     ) && remotecall_wait(() -> reconnect!(CPU), workers()[1])
-    # CImGui.Indent()
-    cts = values(remotecall_fetch(() -> CPU.controllers, workers()[1]))
-    if isempty(cts)
+    if isempty(cpuinfo[:controllers])
         CImGui.TextDisabled(stcstr("(", mlstr("Null"), ")"))
     else
-        taskhandlers = remotecall_fetch(() -> CPU.taskhandlers, workers()[1])
-        instrs = remotecall_fetch(() -> CPU.instrs, workers()[1])
-        for ct in cts
-            @trypass showct(ct, taskhandlers[ct.addr], QInsControlCore.isconnected(instrs[ct.addr])) nothing
-            CImGui.Spacing()
+        instrtocontrollers = Dict()
+        for ct in cpuinfo[:controllers]
+            haskey(instrtocontrollers, ct.instrnm) || push!(instrtocontrollers, ct.instrnm => Dict())
+            haskey(instrtocontrollers[ct.instrnm], ct.addr) || push!(instrtocontrollers[ct.instrnm], ct.addr => [])
+            push!(instrtocontrollers[ct.instrnm][ct.addr], ct)
+        end
+        for (ins, inses) in instrtocontrollers
+            CImGui.TextColored(MORESTYLE.Colors.HighlightText, stcstr(mlstr("Instrument"), " : ", ins))
+            CImGui.Indent()
+            for (addr, cts) in inses
+                CImGui.Text(addr)
+                CImGui.SameLine()
+                CImGui.TextColored(
+                    cpuinfo[:isconnected][addr] ? MORESTYLE.Colors.LogInfo : MORESTYLE.Colors.LogError,
+                    mlstr(cpuinfo[:isconnected][addr] ? "Connected" : "Unconnected")
+                )
+                CImGui.Text(stcstr(mlstr("Status"), "："))
+                CImGui.SameLine()
+                CImGui.TextColored(
+                    cpuinfo[:taskhandlers][addr] ? MORESTYLE.Colors.LogInfo : MORESTYLE.Colors.LogError,
+                    mlstr(cpuinfo[:taskhandlers][addr] ? "Running" : "Stopped")
+                )
+                CImGui.SameLine()
+                CImGui.TextColored(
+                    cpuinfo[:tasksfailed][addr] ? MORESTYLE.Colors.LogError : MORESTYLE.Colors.LogInfo,
+                    mlstr(cpuinfo[:tasksfailed][addr] ? "Failed" : "Well")
+                )
+                CImGui.Indent()
+                for ct in cts
+                    idx = findfirst(==(ct), cpuinfo[:controllers])
+                    CImGui.Text(stcstr(mlstr("Controller"), " ", idx))
+                    @cstatic cols::Cint = 2 begin
+                        CImGui.PushItemWidth(6CImGui.GetFontSize())
+                        @c CImGui.DragInt(mlstr("Buffer"), &cols, 1, 1, 64, "%d", CImGui.ImGuiSliderFlags_AlwaysClamp)
+                        CImGui.PopItemWidth()
+                        CImGui.BeginTable(stcstr("Controller", idx), cols, CImGui.ImGuiTableFlags_Borders)
+                        for idxes in Iterators.partition(eachindex(ct.databuf), cols)
+                            CImGui.TableNextRow()
+                            for i in idxes
+                                CImGui.TableSetColumnIndex((i - 1) % cols)
+                                CImGui.Text(ct.databuf[i])
+                                ct.available[i] || CImGui.TableSetBgColor(
+                                    CImGui.ImGuiTableBgTarget_CellBg,
+                                    CImGui.ColorConvertFloat4ToU32(MORESTYLE.Colors.LogError)
+                                )
+                            end
+                        end
+                        CImGui.EndTable()
+                    end
+                end
+                CImGui.Unindent()
+            end
+            CImGui.Unindent()
         end
     end
-    # CImGui.Unindent()
     CImGui.Unindent()
-    # end
-    # CImGui.End()
-end
-
-function showct(ct::Controller, running, connected)
-    CImGui.TextColored(MORESTYLE.Colors.HighlightText, "ID: ")
-    CImGui.SameLine()
-    CImGui.Text(stcstr(ct.id))
-    CImGui.TextColored(MORESTYLE.Colors.HighlightText, mlstr("Status"))
-    CImGui.Indent()
-    if running
-        CImGui.TextColored(MORESTYLE.Colors.LogInfo, mlstr("Running"))
-    else
-        CImGui.TextColored(MORESTYLE.Colors.LogError, mlstr("Stopped"))
-    end
-    CImGui.Unindent()
-    CImGui.TextColored(MORESTYLE.Colors.HighlightText, mlstr("Instrument"))
-    CImGui.Indent()
-    CImGui.Text(ct.instrnm)
-    # CImGui.SameLine(0, 4CImGui.GetFontSize())
-    CImGui.Text(ct.addr)
-    CImGui.SameLine()
-    if connected
-        CImGui.TextColored(MORESTYLE.Colors.LogInfo, mlstr("Connected"))
-    else
-        CImGui.TextColored(MORESTYLE.Colors.LogError, mlstr("Unconnected"))
-    end
-    CImGui.Unindent()
-    # CImGui.TextColored(MORESTYLE.Colors.HighlightText, mlstr("buffer"))
-    # CImGui.Indent()
-    # if isempty(ct.databuf)
-    #     CImGui.TextDisabled(stcstr("(", mlstr("null"), ")"))
-    # else
-    #     for (id, dt) in ct.databuf
-    #         CImGui.Text(stcstr(id, " => ", dt))
-    #     end
-    # end
-    # CImGui.Unindent()
 end
