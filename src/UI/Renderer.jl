d51aae618ed9297f3c94f50c2457b5bc96519e9c
FaresX <fyzxst@sina.com>
2024-06-29 18:00:51 +0800

    ui on interactive thread

--- src/UI/Renderer.jl
+++ src/UI/Renderer.jl
@@ -164,7 +164,7 @@ function UI(breakdown=false)
 
     breakdown && closeallwindow()
 
-    @async try
+    Threads.@spawn :interactive try
         scale_old::Cfloat = 0
         isshowapp()[] = true
         updateframe::Bool = true
