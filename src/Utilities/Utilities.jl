6e7a188170889bc0b2ea55dc342370af3505fabb
FaresX <fyzxst@sina.com>
2024-10-09 14:26:57 +0800

    merge some files to Utilities folder

--- src/Utilities/Utilities.jl
+++ src/Utilities/Utilities.jl
@@ -262,26 +262,6 @@ function uniformy!(y, z)
     end
 end
 
-mutable struct LoopVector{T}
-    data::Vector{T}
-    index::Integer
-    LoopVector{T}(::UndefInitializer, len) where {T} = new(Vector{T}(undef, len), 1)
-    LoopVector(vec::Vector{T}) where {T} = new{T}(vec, 1)
-end
-
-Base.length(lv::LoopVector) = length(lv.data)
-function __find_index(lv::LoopVector, i)
-    l = length(lv)
-    r = (i + lv.index) % l |> abs
-    return r == 0 ? l : r
-end
-Base.getindex(lv::LoopVector, i=0) = lv.data[__find_index(lv, i)]
-Base.setindex!(lv::LoopVector, x, i=0) = (lv.data[__find_index(lv, i)] = x)
-
-move!(lv::LoopVector, i=1) = (lv.index += i)
-
-Base.push!(lv::LoopVector, x) = push!(lv.data, x)
-
 function waittofetch(f, timeout=2; pollint=0.001)
     waittask = errormonitor(@async fetch(f))
     isok = timedwait(() -> istaskdone(waittask), timeout; pollint=pollint)
