0e31f411ce414a4a7d8166a556a07a60bdf1d220
FaresX <fyzxst@sina.com>
2024-12-23 22:35:26 +0800

    discard "no clearing buffer for GPIB"

--- src/QInsControlCore/Instruments.jl
+++ src/QInsControlCore/Instruments.jl
@@ -238,20 +238,18 @@ determine if the instrument is connected.
 isconnected(instr::Instrument) = instr.connected[]
 
 function clearbuffer(instr::Instrument)
-    if instr isa SerialInstr || instr isa TCPSocketInstr || (instr isa VISAInstr && occursin("ASRL", instr.addr))
+    try
+        read(instr)
+    catch
+    end
+    i = 0
+    while i < 6
         try
             read(instr)
         catch
+            break
         end
-        i = 0
-        while i < 6
-            try
-                read(instr)
-            catch
-                break
-            end
-            i += 1
-            yield()
-        end
+        i += 1
+        yield()
     end
 end
