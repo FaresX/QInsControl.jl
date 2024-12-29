5e2e892fac04335387cadbf0eb7af00add95c0d4
FaresX <fyzxst@sina.com>
2024-12-30 00:32:23 +0800

    manual updating button

--- src/UI/ShowAbout.jl
+++ src/UI/ShowAbout.jl
@@ -1,5 +1,6 @@
 let
     newversion::String = ""
+    hasnewversiontime::Float64 = 0
     global function ShowAbout()
         if CImGui.BeginPopupModal(mlstr("About"), C_NULL, CImGui.ImGuiWindowFlags_AlwaysAutoResize)
             ftsz = CImGui.GetFontSize()
@@ -11,11 +12,20 @@ let
             CImGui.TextColored(MORESTYLE.Colors.HighlightText, "QInsControl\n")
             CImGui.PopFont()
             CImGui.Text(stcstr(mlstr("version"), " : ", QINSCONTROLVERSION))
+            CImGui.SameLine()
             if SYNCSTATES[Int(NewVersion)]
-                CImGui.SameLine()
                 CImGui.TextColored(MORESTYLE.Colors.HighlightText, mlstr("New version!"))
                 CImGui.SameLine()
                 CImGui.TextLinkOpenURL(newversion, "https://github.com/FaresX/QInsControl.jl/releases/latest")
+            else
+                CImGui.SetWindowFontScale(0.6)
+                ColoredButton(MORESTYLE.Icons.Update; colbt=[0, 0, 0, 0]) && (getnewestversion(); hasnewversiontime = time())
+                CImGui.SetWindowFontScale(1)
+                if !SYNCSTATES[Int(NewVersion)] && time() - hasnewversiontime < 4
+                    CImGui.SameLine()
+                    CImGui.SetCursorPosY(CImGui.GetCursorPosY() - 4)
+                    CImGui.TextColored(MORESTYLE.Colors.InfoText, mlstr("Already the latest version!"))
+                end
             end
             CImGui.Text(stcstr(mlstr("author"), " : XST\n"))
             CImGui.Text(stcstr(mlstr("license"), " : MPL-2.0 License\n"))
